ec36150fa6c7edd50175dd614964996945701d0c
srujan uppalapu <srujauppalapu27@gmail.com>
2026-02-27 10:10:03 +0530

    UC9:Recursive Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,28 +1,25 @@
-import java.util.LinkedList;
 public class PalindromeCheckerApp {
+
+    // Recursive method to check palindrome
+    public static boolean isPalindrome(String str, int left, int right) {
+        // Base case: if pointers cross or meet
+        if (left >= right) {
+            return true;
+        }
+
+        // If characters don't match, not a palindrome
+        if (str.charAt(left) != str.charAt(right)) {
+            return false;
+        }
+
+        // Recursive call with next inner characters
+        return isPalindrome(str, left + 1, right - 1);
+    }
+
     public static void main(String[] args) {
         String str = "madam"; // Hardcoded string
 
-        LinkedList<Character> list = new LinkedList<>();
-        boolean isPalindrome = true;
-
-        // Add characters to the linked list
-        for (char ch : str.toCharArray()) {
-            list.add(ch);
-        }
-
-        // Compare characters from both ends
-        while (list.size() > 1) {
-            char first = list.removeFirst();
-            char last = list.removeLast();
-
-            if (first != last) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
+        if (isPalindrome(str, 0, str.length() - 1)) {
             System.out.println("The string \"" + str + "\" is a palindrome.");
         } else {
             System.out.println("The string \"" + str + "\" is not a palindrome.");
