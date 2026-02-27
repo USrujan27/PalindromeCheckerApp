76f094d7f60632e2d8532d5602bdd7d2d6159634
srujan uppalapu <srujauppalapu27@gmail.com>
2026-02-27 09:19:39 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args)
+    {
+        System.out.println("hlooo");
+        System.out.println("this is Palindrome Checker App");
+        System.out.println("ty");
+    }
+
 }
