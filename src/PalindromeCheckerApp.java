550fecbda978d75f4673dfe897d69abd80a5109d
srujan uppalapu <srujauppalapu27@gmail.com>
2026-02-27 09:31:01 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,9 +2,23 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args)
     {
-        System.out.println("hlooo");
-        System.out.println("this is Palindrome Checker App");
-        System.out.println("ty");
+        String str = "madam"; // Hardcoded string
+        boolean isPalindrome = true;
+
+        int len = str.length();
+
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string \"" + str + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + str + "\" is not a palindrome.");
+        }
     }
 
 }
